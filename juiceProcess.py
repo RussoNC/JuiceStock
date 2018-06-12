dc39003b83f3f66bee52ee4061935c66897bdc5f
ext-rrusselo <ext-rrusselo@eramet-sln.nc>
2018-06-12 15:52:49 +1100

    refactor(all): renamed files

--- juiceProcess.py
+++ juiceProcess.py
@@ -3,7 +3,8 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
-class sqlProcess:
-    def __init__(self):
-        self.addrs = ""
+def connect():
+    #Create a engine for connecting to SQLite3.
+    e = create_engine('sqlite:///salaries.db', echo=True)
+
         
